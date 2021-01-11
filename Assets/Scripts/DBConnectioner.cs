4853a1d1cf3d89f1b5b6d371ec1a489417240080
BananaQueen <blackmjack9@gmail.com>
2021-01-11 17:18:04 +0100

    Initial commit

---
+++ Assets/Scripts/DBConnectioner.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Mono.Data.Sqlite;
+using System.Data;
+
+public class DBConnectioner : MonoBehaviour 
+{ 
+
+    private string dbName = "Uri=file:Futile.db";
+
+
+    void Start()
+    {
+        ConnectToDB();
+    }
+
+    public void ConnectToDB()
+    {
+
+        using (var connection = new SqliteConnection(dbName))
+        {
+            connection.Open();
+        }
+    }
+}
+
+
+//Kod do wpisania komendy do bazy danych i wyświetlenia ich
+
+//                using (var command = connection.CreateCommand())
+//{
+//    command.CommandText = "SELECT * FROM Map";
+
+//    using (IDataReader reader = command.ExecuteReader())
+//    {
+//        while (reader.Read())
+//        {
+//            Debug.Log(reader["name"] + "\t\t" + reader["numberOfPlaces"]);
+//            reader.Close();
+//        }
+//    }
+//}
